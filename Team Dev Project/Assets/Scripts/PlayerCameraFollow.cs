f2f4abb108e1eac9c3e3a551ac0dd1fb3200233f
Demix361 <ti-aminov@yandex.ru>
2021-02-27 12:37:18 +0300

    set camera follow

---
+++ Team Dev Project/Assets/Scripts/PlayerCameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using Mirror;
+using Cinemachine;
+using UnityEngine;
+
+public class PlayerCameraFollow : NetworkBehaviour
+{
+    private ICinemachineCamera iVcam;
+
+    [Client]
+    void Start()
+    {
+        iVcam = Camera.main.GetComponent<CinemachineBrain>().ActiveVirtualCamera;
+
+        if (iVcam.Follow == null)
+        {
+            iVcam.Follow = transform;
+        }
+    }
+}
