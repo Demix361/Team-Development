df17838b656d9c27a3cad12e2f38c331e1d79ed6
Demix361 <ti-aminov@yandex.ru>
2021-03-28 01:04:09 +0300

    #7 flag sprite & animations added

---
+++ Team Dev Project/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private PlayerProperties playerProperties;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("FLAG FLAG");
+        playerProperties = collision.GetComponent<PlayerProperties>();
+
+        if (playerProperties.allowInput && (Input.GetButtonDown("Interact") || Input.GetButton("Interact")))
+        {
+            animator.SetBool("Set", true);
+        }
+    }
+}
