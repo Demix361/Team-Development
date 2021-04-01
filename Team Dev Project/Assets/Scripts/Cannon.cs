50785166b5ff44be24a082e751944db19a2622d6
Demix361 <ti-aminov@yandex.ru>
2021-04-01 21:02:23 +0300

    #36 Cannons added

---
+++ Team Dev Project/Assets/Scripts/Cannon.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Cannon : NetworkBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private GameObject _ballPrefab;
+    [SerializeField] private float _shootInterval;
+    [SerializeField] private float _shootOffset;
+    [SerializeField] private float _shootForce;
+    [SerializeField] private Transform _ballSpawnPoint;
+
+    private float count = 0;
+
+    [Command(ignoreAuthority = true)]
+    private void CmdShoot()
+    {
+        RpcShoot();
+    }
+
+    [ClientRpc]
+    private void RpcShoot()
+    {
+        GameObject ball = Instantiate(_ballPrefab, _ballSpawnPoint.position, Quaternion.identity);
+        ball.GetComponent<Rigidbody2D>().AddForce(new Vector2(_shootForce * -transform.localScale.x, 0));
+        //ball.GetComponent<CannonBall>().Move(_shootForce * -transform.localScale.x);
+        _animator.SetTrigger("Fire");
+    }
+
+    private void Update()
+    {
+        if (isServer)
+        {
+            count += Time.deltaTime;
+            if (count > _shootInterval)
+            {
+                count = 0;
+                CmdShoot();
+            }
+        }
+    }
+
+
+}
