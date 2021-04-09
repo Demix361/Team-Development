2bb92d59b323d08baadb438c87bb55610f09defa
Demix361 <ti-aminov@yandex.ru>
2021-04-09 04:05:17 +0300

    #12 player movement improved

--- Team Dev Project/Assets/Scripts/MyPlayerMovement.cs
+++ Team Dev Project/Assets/Scripts/MyPlayerMovement.cs
@@ -4,52 +4,40 @@ using Mirror;
 
 public class MyPlayerMovement : NetworkBehaviour
 {
-    public float runSpeed = 40f;
-    float horizontalMove = 0f;
-    bool jump = false;
-    
-    [SerializeField] private PlayerProperties playerProperties;
-
-    [SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
-    [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;          // Amount of maxSpeed applied to crouching movement. 1 = 100%
-    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
-    [SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
-    [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
-    [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
-    [SerializeField] private Transform m_CeilingCheck;                          // A position marking where to check for ceilings
-    [SerializeField] private Collider2D m_CrouchDisableCollider;                // A collider that will be disabled when crouching
-
-    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-    private bool m_Grounded;            // Whether or not the player is grounded.
-    const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
-    private Rigidbody2D m_Rigidbody2D;
-    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
-    private Vector3 m_Velocity = Vector3.zero;
+    [SerializeField] private float _runSpeed;
+    [SerializeField] private float _jumpForce;
+    [Range(0, .3f)] [SerializeField] private float _movementSmoothing = .05f;
+    [SerializeField] private bool _airControl;
+    [SerializeField] private LayerMask _whatIsGround; // A mask determining what is ground to the character
+    [SerializeField] private Transform _groundCheck; // A position marking where to check if the player is grounded.
+    [SerializeField] private Transform _ceilingCheck; // A position marking where to check for ceilings
 
     [Header("Events")]
-    [Space]
-
     public UnityEvent OnLandEvent;
 
-    [System.Serializable]
-    public class BoolEvent : UnityEvent<bool> { }
+    //[System.Serializable]
+    //public class BoolEvent : UnityEvent<bool> { }
 
-    public BoolEvent OnCrouchEvent;
-    private bool m_wasCrouching = false;
+    [Header("Player Components")]
+    [SerializeField] private PlayerProperties _playerProperties;
+    [SerializeField] private Animator _animator;
 
-    public Animator animator;
-    
+    private float _horizontalMove = 0f;
+    private bool _jump = false;
+    private const float _groundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    private bool _grounded;            // Whether or not the player is grounded.
+    private Rigidbody2D _rigidbody;
+    private bool _facingRight = true;  // For determining which way the player is currently facing.
+    private Vector3 _velocity = Vector3.zero;
+
 
     [Client]
     private void Awake()
     {
-        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
 
         if (OnLandEvent == null)
             OnLandEvent = new UnityEvent();
-
-        if (OnCrouchEvent == null)
-            OnCrouchEvent = new BoolEvent();
     }
 
     void Update()
@@ -59,88 +47,88 @@ public class MyPlayerMovement : NetworkBehaviour
             return;
         }
 
-        if (playerProperties.allowInput)
+        if (_playerProperties.allowInput)
         {
-            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+            _horizontalMove = Input.GetAxisRaw("Horizontal") * _runSpeed;
         }
         else
         {
-            horizontalMove = 0;
+            _horizontalMove = 0;
         }
         
-        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        _animator.SetFloat("Speed", Mathf.Abs(_horizontalMove));
         
 
-        if (playerProperties.allowInput && Input.GetButtonDown("Jump"))
+        if (_playerProperties.allowInput && Input.GetButtonDown("Jump"))
         {
-            jump = true;
-            animator.SetBool("IsJumping", true);
+            _jump = true;
+            _animator.SetBool("IsJumping", true);
         }
     }
 
     public void OnLanding()
     {
-        animator.SetBool("IsJumping", false);
+        _animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate()
     {
-        bool wasGrounded = m_Grounded;
-        m_Grounded = false;
+        bool wasGrounded = _grounded;
+        _grounded = false;
 
         // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
         // This can be done using layers instead but Sample Assets will not overwrite your project settings.
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(_groundCheck.position, _groundedRadius, _whatIsGround);
         for (int i = 0; i < colliders.Length; i++)
         {
-            if (colliders[i].gameObject != gameObject)
+            if (!colliders[i].isTrigger && colliders[i].gameObject != gameObject)
             {
-                m_Grounded = true;
+                _grounded = true;
                 if (!wasGrounded)
                     OnLandEvent.Invoke();
             }
         }
 
-        Move(horizontalMove * Time.fixedDeltaTime, false, jump);
-        jump = false;
+        Move(_horizontalMove * Time.fixedDeltaTime, _jump);
+        _jump = false;
     }
 
-    public void Move(float move, bool crouch, bool jump)
+    public void Move(float move, bool jump)
     {
         //only control the player if grounded or airControl is turned on
-        if (m_Grounded || m_AirControl)
+        if (_grounded || _airControl)
         {
             // Move the character by finding the target velocity
-            Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+            Vector3 targetVelocity = new Vector2(move * 10f, _rigidbody.velocity.y);
             // And then smoothing it out and applying it to the character
-            m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+            _rigidbody.velocity = Vector3.SmoothDamp(_rigidbody.velocity, targetVelocity, ref _velocity, _movementSmoothing);
 
             // If the input is moving the player right and the player is facing left...
-            if (move > 0 && !m_FacingRight)
+            if (move > 0 && !_facingRight)
             {
                 // ... flip the player.
                 Flip();
             }
             // Otherwise if the input is moving the player left and the player is facing right...
-            else if (move < 0 && m_FacingRight)
+            else if (move < 0 && _facingRight)
             {
                 // ... flip the player.
                 Flip();
             }
         }
         // If the player should jump...
-        if (m_Grounded && jump)
+        if (_grounded && jump)
         {
             // Add a vertical force to the player.
-            m_Grounded = false;
-            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+            _grounded = false;
+            _rigidbody.AddForce(new Vector2(0f, _jumpForce));
         }
     }
 
     private void Flip()
     {
         // Switch the way the player is labelled as facing.
-        m_FacingRight = !m_FacingRight;
+        _facingRight = !_facingRight;
 
         // Multiply the player's x local scale by -1.
         Vector3 theScale = transform.localScale;
