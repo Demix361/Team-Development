e17682ea091e1f2165cbeedf5960e1b72ccdd01f
sibastr <49680365+sibastr@users.noreply.github.com>
2021-04-21 23:27:14 +0300

    #49 PlayerSpawnPoint Документирован

--- Team Dev Project/Assets/Scripts/PlayerSpawnPoint.cs
+++ Team Dev Project/Assets/Scripts/PlayerSpawnPoint.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
-
+/// <summary>
+/// Класс точки появление персонажей игроков.
+/// </summary>
+/// <remarks>
+/// Добавляет и удаляет точки появления персонажей игроков.
+/// </remarks>
 public class PlayerSpawnPoint : MonoBehaviour
 {
+    /// <summary>
+    /// Добавляет точки появления персонажей игроков.
+    /// </summary>
     private void Awake()
     {
         PlayerSpawnSystem.AddSpawnPoint(transform);
     }
-
+    /// <summary>
+    /// Удаляет точки появления персонажей игроков.
+    /// </summary>
     private void OnDestroy()
     {
         PlayerSpawnSystem.RemoveSpawnPoint(transform);
     }
-
+    
     void OnDrawGizmos()
     {
         float radius = (float)0.3;
