1fa95f8afb83e5ac48262ca98e32d16ac6881d50
sibastr <49680365+sibastr@users.noreply.github.com>
2021-04-21 23:01:19 +0300

    #49 Popup документирован

--- Team Dev Project/Assets/Scripts/Popup.cs
+++ Team Dev Project/Assets/Scripts/Popup.cs
@@ -1,19 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Класс всплывающего уведомления.
+/// </summary>
+/// <remarks>
+/// Начинает и заканчивает отсчет перед всплывающим уведомлением. Вызывает всплывающее уведомление.
+/// </remarks>
 public class Popup : MonoBehaviour
 {
+    /// <summary>
+    /// Значение задержки перед уведомлением
+    /// </summary>
     [SerializeField] private float popupDelay;
+    /// <summary>
+    /// Объект появляющийся при уведомлении.
+    /// </summary>
     [SerializeField] private GameObject popupSprite;
+    /// <summary>
+    /// Счетчик.
+    /// </summary>
     private float counter = 0;
+    /// <summary>
+    /// Состояние начала отчета.
+    /// </summary>
     private bool needCount;
-
+    /// <summary>
+    /// Объявляет значение состояния счетчика.
+    /// </summary>
     private void Start()
     {
         needCount = false;
     }
-
+    /// <summary>
+    /// Обновляет значение состояния счетчика.
+    /// </summary>
+    /// <param name="state">Состояние игрового персонажи</param>
     public void SetPopup(bool state)
     {
         if (state)
@@ -27,7 +49,9 @@ public class Popup : MonoBehaviour
             counter = 0;
         }
     }
-
+    /// <summary>
+    /// Обновляет значение счетчика и состояния счетчика.
+    /// </summary>
     private void Update()
     {
         if (needCount)
