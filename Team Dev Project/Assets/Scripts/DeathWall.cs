cd0a4ee8790df67fd1ffdafd305be51f3dd3345d
Demix361 <ti-aminov@yandex.ru>
2021-04-21 18:03:56 +0300

    #49 DeathWall документирован

--- Team Dev Project/Assets/Scripts/DeathWall.cs
+++ Team Dev Project/Assets/Scripts/DeathWall.cs
@@ -1,11 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// Класс стены смерти
+/// </summary>
+/// <remarks>
+/// Убивает игрока при столкновении с собой
+/// </remarks>
 public class DeathWall : MonoBehaviour
 {
+    /// <summary>
+    /// BoxCollider2D стены
+    /// </summary>
     [SerializeField] private BoxCollider2D boxCollider;
 
+    /// <summary>
+    /// Наносит максимальный урон игроку при столкновении.
+    /// </summary>
+    /// <param name="collision">Collider2D объекта, вошедшего в триггер.</param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
         collision.GetComponent<Health>().CmdDealMaxDamage();
@@ -13,11 +24,9 @@ public class DeathWall : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        
         Gizmos.color = Color.red;
         Vector3 left = new Vector3(transform.position.x - boxCollider.size.x / 2, transform.position.y, 0);
         Vector3 right = new Vector3(transform.position.x + boxCollider.size.x / 2, transform.position.y, 0);
         Gizmos.DrawLine(left, right);
-        
     }
 }
