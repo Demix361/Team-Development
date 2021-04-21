32f99d478eb427ea81c1cb7b1eca1930699245f1
sibastr <49680365+sibastr@users.noreply.github.com>
2021-04-22 01:33:45 +0300

    #49 Документированы скрипты(смотри описание)
    Задокументированы скрипты(MyPlayerMovement,Pearl,PlayerAttack,PlayerCamerFollow)

--- Team Dev Project/Assets/Scripts/PlayerAttack.cs
+++ Team Dev Project/Assets/Scripts/PlayerAttack.cs
@@ -3,18 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
+/// <summary>
+/// Класс атаки игрока.
+/// </summary>
+/// <remarks>
+/// Игрок бьет с заданым уроном и интервалом.
+/// </remarks>
 public class PlayerAttack : NetworkBehaviour
 {
+    /// <summary>
+    /// Свойства игрока
+    /// </summary>
     [SerializeField] private PlayerProperties _playerProperties;
+    /// <summary>
+    /// Аниматор атаки.
+    /// </summary>
     [SerializeField] private Animator _animator;
+    /// <summary>
+    /// Трансформ атаки.
+    /// </summary>
     [SerializeField] private Transform _attackColliderPoint1;
+    /// <summary>
+    /// Трансфом атаки.
+    /// </summary>
     [SerializeField] private Transform _attackColliderPoint2;
+    /// <summary>
+    /// Маска уровней.
+    /// </summary>
     [SerializeField] private LayerMask _layerMask;
+    /// <summary>
+    ///  Урон атаки.
+    /// </summary>
     [SerializeField] private float _damage;
+    /// <summary>
+    /// Интервал между атаками.
+    /// </summary>
     [SerializeField] private float _interval;
 
+    /// <summary>
+    /// Счетчик.
+    /// </summary>
     private float _count = 0;
 
+    /// <summary>
+    /// Нанесение урона и отталкивание врага при атаке и попадании
+    /// </summary>
     private void Update()
     {
         if (!hasAuthority)
@@ -38,12 +71,21 @@ public class PlayerAttack : NetworkBehaviour
             }
         }
     }
-    
+
+    /// <summary>
+    /// Окрашивание в красный при попадании
+    /// </summary>
     private void OnDrawGizmos()
     {
         DrawRect(_attackColliderPoint1, _attackColliderPoint2, Color.red);
     }
 
+    /// <summary>
+    /// Изменение цветов
+    /// </summary>
+    /// <param name="point1">Transform точки1</param>
+    /// <param name="point2">Transform точки2</param>
+    /// <param name="color">Цвет</param>
     private void DrawRect(Transform point1, Transform point2, Color color)
     {
         Gizmos.color = color;
