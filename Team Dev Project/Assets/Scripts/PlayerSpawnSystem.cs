57e44032e3d33da5d8981acd603429134e80550c
sibastr <49680365+sibastr@users.noreply.github.com>
2021-04-21 23:16:52 +0300

    #49 PlayerSpawnSystem документирован

--- Team Dev Project/Assets/Scripts/PlayerSpawnSystem.cs
+++ Team Dev Project/Assets/Scripts/PlayerSpawnSystem.cs
@@ -2,12 +2,26 @@
 using Mirror;
 using System.Collections.Generic;
 using System.Linq;
-
+/// <summary>
+/// Класс системы появления игровых персонажей.
+/// </summary>
 public class PlayerSpawnSystem : NetworkBehaviour
 {
+    /// <summary>
+    /// Prefab игрока.
+    /// </summary>
     [SerializeField] private GameObject playerPrefab;
+    /// <summary>
+    /// Список мест появлений игроков.
+    /// </summary>
     public static List<Transform> spawnPoints = new List<Transform>();
+    /// <summary>
+    /// Transform точки смерти персонажа.
+    /// </summary>
     public static Transform deathPoint;
+    /// <summary>
+    /// Значение следующего индекса.
+    /// </summary>
     private int nextIndex = 0;
 
     private MyNetworkManager room;
@@ -23,32 +37,48 @@ public class PlayerSpawnSystem : NetworkBehaviour
             return room = NetworkManager.singleton as MyNetworkManager;
         }
     }
-
+    /// <summary>
+    /// Добавление точек появляние персонажей игроков.
+    /// </summary>
+    /// <param name="transform">Transform точки</param>
     public static void AddSpawnPoint(Transform transform)
     {
         spawnPoints.Add(transform);
 
         spawnPoints = spawnPoints.OrderBy(x => x.GetSiblingIndex()).ToList();
     }
-
+    /// <summary>
+    /// Удаление точек появление персонажей игроков.
+    /// </summary>
+    /// <param name="transform">Transform точки</param>
     public static void RemoveSpawnPoint(Transform transform)
     {
         spawnPoints.Remove(transform);
     }
-
+    /// <summary>
+    /// Добавление точек смерти персонажей игроков.
+    /// </summary>
+    /// <param name="transform">Transform точки</param>
     public static void AddDeathPoint(Transform transform)
     {
         deathPoint = transform;
     }
-
+    /// <summary>
+    /// Добавление функции обратного вызова.
+    /// </summary>
     public override void OnStartServer()
     {
         MyNetworkManager.OnServerReadied += SpawnPlayer;
     }
-
+    /// <summary>
+    /// Удаление функции обратного вызова.
+    /// </summary>
     [ServerCallback]
     private void OnDestory() => MyNetworkManager.OnServerReadied -= SpawnPlayer;
-
+    /// <summary>
+    /// Появление персонажей игроков.
+    /// </summary>
+    /// <param name="conn">Значение conn персонажа игрока</param>
     [Server]
     public void SpawnPlayer(NetworkConnection conn)
     {
